7b18e1e689119f4d38bdc08c1e850829ecd3c3b7
dkAsCambodia <nitindamo50@gmail.com>
2025-07-30 11:31:47 +0700

    setup RichPay Deposit transaction page design

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\RichPayController;
 
 use App\Livewire\Auth\LoginPage;
 use App\Livewire\DashboardPage;
@@ -33,5 +34,16 @@ Route::get('/invoice-qrcode-list', QrcodeList::class)->name('invoice.qrcode.list
 // Route::get('fc/r2pdeposit/{amount}/{invoice_number}/{customer_name}', 'fcs2pDeposit')->name('fc.s2p.Deposit');
 
 Route::get('/fc/r2pdeposit/{amount}/{invoice_number}/{customer_name}', DepositFormRichpay::class)->name('fc.r2pdeposit');
+
+Route::controller(RichPayController::class)->group(function () {
+    Route::get('/r2pPayin', 'payinform');
+    Route::get('/r2pPaymentPage/{frtransaction}', 'paymentPage');
+    Route::get('/r2pPaymentPage2/{frtransaction}', 'paymentProcessingPage');
+    Route::get('r2p/payinResponse/{frtransaction}', 'payinResponse');
+    Route::get('/r2pPayout', 'r2pPayoutform'); 
+
+    Route::get('/r2p/payintest', 'payintest'); 
+    Route::get('/r2p/payouttest', 'payouttest'); 
+});
 
  
