0bb80500aa49182c59138ee8d9ef49993f2e8d2f
dkAsCambodia <nitindamo50@gmail.com>
2025-08-15 11:15:35 +0700

    set timezone Asia/Bangkok in configuration

--- resources/views/components/layouts/master.blade.php
+++ resources/views/components/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <title>{{ $title ?? 'PAYMENT GATEWAY' }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Responsive bootstrap 4 admin template" name="description" />
+        <meta content="International Payment Gateway System" name="description" />
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
