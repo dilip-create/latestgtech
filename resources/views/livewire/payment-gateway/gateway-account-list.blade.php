e6c3e0e2415401f054bb405a8de68da45770cbbf
dkAsCambodia <nitindamo50@gmail.com>
2025-08-21 13:30:06 +0700

    create edit popup and add popup record for gateway account menu : Done

--- resources/views/livewire/payment-gateway/gateway-account-list.blade.php
+++ resources/views/livewire/payment-gateway/gateway-account-list.blade.php
@@ -21,7 +21,9 @@
         <div class="row">
             <div class="col-12">
                 <div class="card-box">
-                    {{-- <a href="#custom-modal" class="view-record-btn btn btn-success waves-effect waves-light float-right" data-animation="door" data-plugin="custommodal" data-overlayspeed="100" data-overlaycolor="#36404a"><i class="fas fa-plus"></i> {{ __('messages.Add Gateway Account') }}</a> --}}
+                    <a href="#" wire:click.prevent="openAddModal" class="btn btn-success waves-effect waves-light float-right mb-3">
+                        <i class="fas fa-plus"></i> {{ __('messages.Add Gateway Account') }}
+                    </a>
                     <table  class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                         <thead>
                         <tr>
@@ -53,9 +55,11 @@
                                 </label>
                             </td>
                             <td>
-                                {{-- <a href="#custom-modal" class="view-record-btn btn btn-primary waves-effect waves-light" data-id="{{ $row->id }}" data-animation="blur" data-plugin="custommodal"
-                                data-overlaySpeed="100" data-overlayColor="#36404a"><i class="fas fa-pencil-alt"></i></a> --}}
-                                
+                                 <button class="btn btn-primary"
+                            wire:key="edit-{{ $row->id }}"
+                            wire:click.prevent="editConfirmationFun({{ $row->id }})">
+                            <i class="fas fa-pencil-alt"></i>
+                        </button>
                                 <button class="btn btn-danger waves-effect waves-light" wire:key="delete-{{ $row->id }}" wire:click.prevent="deleteConfirmationFun({{ $row->id }})">
                                     <i class="fa fa-trash" aria-hidden="true"></i>
                                 </button>
@@ -74,6 +78,48 @@
             </div>
         </div>
         <!-- end row -->
+<!-- Modal -->
+    @if($showModal)
+    <div class="modal fade show d-block" style="background-color: rgba(0,0,0,0.5);">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{ $isEditMode ? 'Edit Gateway Account' : 'Add Gateway Account' }}</h5>
+                    <button type="button" class="close" wire:click="closeModal">×</button>
+                </div>
+                <form wire:submit.prevent="saveGatewayAccount">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Gateway Name</label>
+                            <input type="text" class="form-control" wire:model="gateway_name">
+                            @error('gateway_name') <span class="text-danger">{{ $message }}</span> @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Website URL</label>
+                            <input type="text" class="form-control" wire:model="website_url">
+                            @error('website_url') <span class="text-danger">{{ $message }}</span> @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Payment Method</label>
+                            {{-- <input type="text" class="form-control" wire:model="payment_method"> --}}
+                            <select wire:model="payment_method" class="form-control custom-select">
+                                <option value="">--Select--</option>
+                                <option value="QR">QR</option>
+                                <option value="card">Card</option>
+                            </select>
+                            @error('payment_method') <span class="text-danger">{{ $message }}</span> @enderror
+                            
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">{{ $isEditMode ? 'Update' : 'Save' }}</button>
+                        <button type="button" class="btn btn-secondary" wire:click="closeModal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    @endif
 
 <script>
     // FOR DELETE RECORD CODE START
