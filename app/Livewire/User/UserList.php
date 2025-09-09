545c61104b8672c70a990a714b3fe67e5001fb49
dkAsCambodia <nitindamo50@gmail.com>
2025-09-09 17:36:19 +0700

    show all user list and add new user, update user functionality status : inprogress

---
+++ app/Livewire/User/UserList.php
@@ -0,0 +1,205 @@
+<?php
+namespace App\Livewire\User;
+use Livewire\Component;
+use App\Models\Agent;
+use App\Models\Merchant;
+use App\Models\User;
+use Livewire\WithDispatchesEvents;
+use Livewire\WithPagination;
+use Session;
+
+class UserList extends Component
+{
+    use WithPagination;
+    public $search = '';        // Searching
+    public $perPage = 10;       // Records per page
+    protected $paginationTheme = 'bootstrap';
+    
+    // FOR EDIT AND ADD RECORD FUNCTIONALITY CODE START
+    public $user_id, $agent_id, $merchant_id, $name, $user_name, $email, $mobile_number, $password, $password_confirmation, $timezone='Asia/Bangkok';
+    public $showModal = false; // To toggle the popup
+    public $isEditMode = false; // To detect if editing or adding
+    public $agentList;
+    public function mount()
+    {
+        $this->agentList = Agent::where('status', '1')->get();
+    }
+
+    /** Open modal for adding new record */
+    public function openAddModal()
+    {
+        $this->resetInputFields();
+        $this->isEditMode = false;
+        $this->showModal = true;
+        if(Session::get('auth')->role_name == 'Agent'){
+            $this->agent_id = Session::get('auth')->agent_id;
+        }
+    }
+
+    /** Open modal for editing */
+    public function editConfirmationFun($id)
+    {
+        $userdata = User::findOrFail($id);
+        $this->user_id = $userdata->id;
+        $this->merchant_id = $userdata->merchant_id;
+        $this->agent_id = $userdata->agent_id;
+        $this->name = $userdata->name;
+        $this->user_name = $userdata->user_name;
+        $this->email = $userdata->email;
+        $this->mobile_number = $userdata->mobile_number;
+
+        $this->isEditMode = true;
+        $this->showModal = true;
+    }
+
+    /** Save or update record */
+    public function saveGatewayAccount()
+    {
+        // no need to check unique at edit case code 
+            $rules = [
+                'agent_id'          => 'required',
+                'name'          => 'required|string|max:255',
+                'user_name'     => 'required|string|max:255|unique:users,user_name,' . $this->user_id,
+                'email'         => 'required|email|max:255|unique:users,email,' . $this->user_id,
+                'mobile_number' => 'required|numeric|digits_between:10,12',
+            ];
+        
+        if ($this->isEditMode) {
+            // Update case → only validate password if filled
+            if (!empty($this->password) || !empty($this->password_confirmation)) {
+                $rules['password'] = 'required|min:8|confirmed';
+            }
+        } else {
+            // Create case → password is required
+            $rules['password'] = 'required|min:8|confirmed';
+        }
+        $this->validate($rules);
+        // dd(base64_encode($this->password));
+         // Save or update Agent
+        $merchant = Merchant::updateOrCreate(
+            ['id' => $this->merchant_id],
+            [
+                'merchant_name' => $this->name,
+                'merchant_code' => $this->user_name,
+                'agent_id' => $this->agent_id,
+            ]
+        );
+
+         $userData = [
+            'name' => $this->name,
+            'user_name' => $this->user_name,
+            'email' => $this->email,
+            'mobile_number' => $this->mobile_number,
+            'role_id' => '4',  //for merchant role
+            'role_name' => 'Merchant',
+            'merchant_id' => $merchant->id,
+            'agent_id' => $merchant->agent_id,
+        ];
+
+        // Only update password if it's filled
+        if (!empty($this->password)) {
+            $userData['password'] = base64_encode($this->password);
+        }
+        
+        User::updateOrCreate(
+            ['id' => $this->user_id],
+            $userData
+        );
+
+
+        $this->getdata();
+        $msg = $this->isEditMode  ? __('messages.Merchant updated successfully!') : __('messages.Merchant added successfully!');
+        $this->dispatch('toast', message: $msg, notify:'success' ); 
+        $this->closeModal();
+    }
+
+    /** Close modal */
+    public function closeModal()
+    {
+        $this->showModal = false;
+    }
+
+    /** Reset input fields */
+    private function resetInputFields()
+    {
+        $this->user_id = null;
+        $this->agent_id = '';
+        // $this->merchant_id = '';
+        $this->name = '';
+        $this->user_name = '';
+        $this->email = '';
+        $this->mobile_number = '';
+        $this->password = '';
+        $this->password_confirmation = '';
+    }
+    // FOR EDIT AND ADD RECORD FUNCTIONALITY CODE END
+
+
+    public function getdata()
+    {
+        $query = User::query()
+            ->where(function ($query) {
+                $query->where('name', 'like', "%{$this->search}%")
+                    ->orWhere('user_name', 'like', "%{$this->search}%")
+                    ->orWhere('email', 'like', "%{$this->search}%")
+                    ->orWhere('mobile_number', 'like', "%{$this->search}%");
+            });
+
+        //  Check if logged-in user is an Agent
+        if (Session::get('auth')->role_name === 'Agent') {
+            $query->where('agent_id', Session::get('auth')->agent_id);
+        }
+
+        return $query->orderBy('id', 'desc')
+            ->paginate($this->perPage);
+    }
+
+
+
+    // FOR CHANGE STATUS CODE START
+    public function toggleStatus($UserId)
+    {
+        $User = User::find($UserId);
+        if ($User) {
+            $User->status = !$User->status;
+            $User->save();
+
+            // For Merchant Table
+           $merchent = Merchant::find($User->merchant_id);
+            if ($merchent) {
+                $merchent->status = $merchent->status ? 0 : 1;
+                $merchent->save();
+            }
+        
+            // Emit an event to trigger SweetAlert in frontend
+            $this->dispatch('status-changed');
+        }
+    }
+    // FOR CHANGE STATUS CODE END
+    // FOR DELETE RECORD CODE START
+    public $record_id;
+    protected $listeners = ['callDeleteListner' => 'delete'];
+    public function deleteConfirmationFun($id){
+      
+        $this->record_id = $id;
+        $this->dispatch('show-delete-confirmation');
+    }
+    public function delete(){
+      
+         $data= User::find($this->record_id);
+         $merchantdata= Merchant::find($data->merchant_id);
+         $merchantdata->delete();
+         $data->delete();
+         $this->getdata();
+         $this->dispatch('recordDeleted');
+    }
+    // FOR DELETE RECORD CODE END
+
+    public function render()
+    {
+        return view('livewire.user.user-list', [
+            'gateways' => $this->getData(),
+        ]);
+    }
+    
+}
---
+++ resources/views/livewire/user/user-list.blade.php
@@ -0,0 +1,248 @@
+<div>
+    <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box">
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="/">GTech</a></li>
+                            <li class="breadcrumb-item active">{{ __('messages.Users List') }}</li>
+                        </ol>
+                    </div>
+                    <h4 class="page-title">{{ __('messages.Users List') }}</h4>
+                </div>
+            </div>
+        </div>     
+        <!-- end page title --> 
+        
+        <div class="row">
+            <div class="col-12">
+                <div class="card-box">
+                    <!-- Search + Per Page -->
+                    <div class="row mb-3">
+                        <div class="col-md-2">
+                            <label>{{ __('messages.Show') }}</label>
+                            <select wire:model.live="perPage" class="form-control">
+                                <option value="2">2</option>
+                                <option value="10">10</option>
+                                <option value="25">25</option>
+                                <option value="50">50</option>
+                            </select>
+                        </div>
+                        <div class="col-md-3">
+                            <label>{{ __('messages.Search') }}</label>
+                            <input type="text" wire:model.live="search" class="form-control" placeholder="{{ __('messages.Search here') }}...">
+                        </div>
+                        <div class="col-md-5"></div>
+                        <div class="col-md-2">
+                            <a href="#" wire:click.prevent="openAddModal" class="btn btn-success waves-effect waves-light float-right mb-3">
+                                <i class="fas fa-plus"></i> {{ __('messages.Add User') }}
+                            </a>
+                        </div>
+                    </div>
+                    <table  class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                        <thead>
+                        <tr>
+                            <th>{{ __('messages.Order Id') }}</th>
+                            <th>{{ __('messages.Merchant Name') }}</th>     
+                            <th>{{ __('messages.Merchant Code') }} </th>
+                           
+                            <th>{{ __('messages.Email') }} </th>
+                            <th>{{ __('messages.Mobile') }} </th>
+                            <th>{{ __('messages.Role Name') }} </th>
+                            <th>{{ __('messages.Created Time') }}</th>
+                            <th>{{ __('messages.Status') }}</th>
+                            <th>{{ __('messages.Action') }}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                             @forelse ($gateways as $index => $row)
+                                <tr wire:key="row-{{ $row->id }}">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ ucfirst($row->name) ?? '' }}</td>
+                                    <td>{{ ucfirst($row->user_name) ?? '' }}</td>
+                                   
+                                    <td>{{ $row->email ?? '' }}</td>
+                                    <td>{{ $row->mobile_number ?? '' }}</td>
+                                    <td>{{ $row->role_name ?? '' }}</td>
+
+                                    <td> {{ $row->created_at ? $row->created_at->format('d-m-Y h:i:s A') : '' }}</td>
+                                    <td>
+                                        <label class="switch">
+                                            <input type="checkbox" wire:click="toggleStatus({{ $row->id }})" {{ $row->status ? 'checked' : '' }}>
+                                            <span class="slider"></span>
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-primary" wire:key="edit-{{ $row->id }}" wire:click.prevent="editConfirmationFun({{ $row->id }})">
+                                            <i class="fas fa-pencil-alt"></i>
+                                        </button>
+                                        <button class="btn btn-danger waves-effect waves-light" wire:key="delete-{{ $row->id }}" wire:click.prevent="deleteConfirmationFun({{ $row->id }})">
+                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td></td><td></td><td></td><td>{{ __('messages.Record not found') }}!</td><td></td><td></td></td><td>
+                                </tr>
+                            @endforelse
+                        
+                        </tbody>
+                    </table>
+                    <!-- Pagination -->
+                        <div class="mt-3">
+                            <span class="h5">{{ $gateways->links() }}</span>
+                        </div>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- end row -->
+<!-- Modal -->
+    @if($showModal)
+        <div class="modal fade show d-block li" style="background-color: rgba(0,0,0,0.5);">
+            <div class="modal-dialog modal-lg"> {{-- Make modal larger --}}
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-center w-100">
+                            {{ $isEditMode ? __('messages.Edit Merchant') : __('messages.Add Merchant') }}
+                        </h5>
+                        <button type="button" class="close" wire:click="closeModal">×</button>
+                    </div>
+
+                    <form wire:submit.prevent="saveGatewayAccount">
+                        <div class="modal-body">
+                            <div class="row">
+                                <!-- Agent Name -->
+                                <div class="col-md-6 mb-3">
+                                    <label>{{ __('messages.Merchant Name') }}<span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" wire:model.live="name" placeholder="{{ __('messages.Enter Merchant Name') }}">
+                                    @error('name') <span class="text-danger">{{ $message }}</span> @enderror
+                                </div>
+
+                                <!-- Agent Code -->
+                                <div class="col-md-6 mb-3">
+                                    <label>{{ __('messages.Select Agent') }}<span class="text-danger">*</span></label>
+                                    <select wire:model="agent_id" class="form-control custom-select" {{ Session::get('auth')->role_name == 'Agent' ?  'disabled' : '' }}>
+                                        <option value="">--{{ __('messages.Select') }}--</option>
+                                        @forelse ($agentList as $index => $agent)
+                                        <option value="{{ $agent->id }}">{{ ucfirst($agent->agent_name) }}</option>
+                                        @empty
+                                        <option value="">{{ __('messages.Record not found') }}!</option>
+                                        @endforelse
+                                    </select>
+                                    @error('agent_id') <span class="text-danger">{{ $message }}</span> @enderror
+                                </div>
+
+                                <!-- Email -->
+                                <div class="col-md-6 mb-3">
+                                    <label>{{ __('messages.Email') }}<span class="text-danger">*</span></label>
+                                    <input type="email" class="form-control" wire:model.live="email" placeholder="{{ __('messages.Enter email') }}">
+                                    @error('email') <span class="text-danger">{{ $message }}</span> @enderror
+                                </div>
+
+                                <!-- Username -->
+                                <div class="col-md-6 mb-3">
+                                    <label>{{ __('messages.Username') }} or {{ __('messages.Merchant Code') }}<span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" wire:model.live="user_name" placeholder="{{ __('messages.Enter here') }}" {{ $isEditMode ? 'disabled' : '' }}>
+                                    @error('user_name') <span class="text-danger">{{ $message }}</span> @enderror
+                                </div>
+
+                                <!-- Mobile Number -->
+                                <div class="col-md-6 mb-3">
+                                    <label>{{ __('messages.Mobile') }}<span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" wire:model.live="mobile_number" placeholder="{{ __('messages.Enter Mobile Number') }}">
+                                    @error('mobile_number') <span class="text-danger">{{ $message }}</span> @enderror
+                                </div>
+
+                                <!-- Timezone -->
+                                <div class="col-md-6 mb-3">
+                                    <label>{{ __('messages.Timezone') }}<span class="text-danger">*</span></label>
+                                    <select class="form-control" wire:model="timezone">
+                                        <option value="">{{ __('messages.Select') }}</option>
+                                        <option value="Asia/Kolkata">Asia/Kolkata</option>
+                                        <option value="Asia/Bangkok">Asia/Bangkok</option>
+                                        <option value="UTC">UTC</option>
+                                        <!-- Add more options as needed -->
+                                    </select>
+                                    @error('timezone') <span class="text-danger">{{ $message }}</span> @enderror
+                                </div>
+
+                                <!-- Password -->
+                                <div class="col-md-6 mb-3">
+                                    <label>{{ __('messages.Password') }}<span class="text-danger">*</span></label>
+                                    <input type="password" class="form-control" wire:model.live="password" placeholder="{{ __('messages.Enter password') }}">
+                                    @error('password') <span class="text-danger">{{ $message }}</span> @enderror
+                                </div>
+
+                                <!-- Confirm Password -->
+                                <div class="col-md-6 mb-3">
+                                    <label>{{ __('messages.Confirm Password') }}<span class="text-danger">*</span></label>
+                                    <input type="password" class="form-control" wire:model.live="password_confirmation" placeholder="{{ __('messages.Enter Confirm Password') }}"  @if(!$password) disabled @endif>
+                                    @error('password_confirmation') <span class="text-danger">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">
+                                {{ $isEditMode ? __('messages.Update') : __('messages.Save') }}
+                            </button>
+                            <button type="button" class="btn btn-secondary" wire:click="closeModal">
+                                {{ __('messages.Close') }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    @endif
+
+<script>
+    // FOR DELETE RECORD CODE START
+        document.addEventListener('DOMContentLoaded', function () {
+              window.addEventListener('show-delete-confirmation', event => {
+                  Swal.fire({
+                      title: "{{ __('messages.Are you sure you want to delete this record?') }}",
+                      text: "{{ __('messages.If you delete this, it will be gone forever') }}!",
+                      icon: "warning",
+                      showCancelButton: true,
+                      confirmButtonColor: "#3085d6",
+                      cancelButtonColor: "#d33",
+                      confirmButtonText: "{{ __('messages.Yes') }}!",
+                      cancelButtonText: "{{ __('messages.No') }}"
+                  }).then((result) => {
+                      if (result.isConfirmed) {
+                          if (typeof Livewire !== 'undefined' && typeof Livewire.dispatch === 'function') {
+                              Livewire.dispatch('callDeleteListner');
+                          } else {
+                              console.error('Livewire is not available or dispatch function is not defined.');
+                          }
+                      }
+                  });
+              });
+          });
+        window.addEventListener('recordDeleted', event => {
+          Swal.fire(
+              "{{ __('messages.Done') }}!",
+              "{{ __('messages.Successfully deleted') }}!",
+              'success'
+            )
+        });
+    // FOR DELETE RECORD CODE END
+    // FOR CHANGE STATUS CODE START
+        window.addEventListener('status-changed', event => {
+            Swal.fire(
+                "{{ __('messages.Done') }}!",
+                "{{ __('messages.Status Changed Successfully!') }}.",
+                'success'
+                )
+        });
+    // FOR CHANGE STATUS CODE END
+    
+</script>            
+
+            
+</div>
