8ea4ec54648f6bbcb9297093d13668627e71cc90
dkAsCambodia <nitindamo50@gmail.com>
2025-09-11 18:36:43 +0700

    Work on Real-Time Notifications with Sound in Laravel status : InProgress

---
+++ database/migrations/2025_09_11_170714_create_transaction_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('notifiable_type')->nullable();  
+            $table->string('agent_id')->nullable();  
+            $table->string('merchant_id')->nullable();  
+            $table->boolean('readby_agent')->default(false);
+            $table->boolean('readby_merchant')->default(false);
+            $table->boolean('readby_admin')->default(false);
+            $table->longText('data')->nullable();  
+            $table->string('msg')->nullable();  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_notifications');
+    }
+};
