91ecd242f466a5a426c59e7ef761ab34faaf3ab4
dkAsCambodia <nitindamo50@gmail.com>
2025-09-12 18:07:20 +0700

    Work on Real-Time Notifications with Sound in Laravel status : InProgress

--- app/Events/DepositCreated.php
+++ app/Events/DepositCreated.php
@@ -37,7 +37,7 @@ use Illuminate\Queue\SerializesModels;
 
 class DepositCreated implements ShouldBroadcast
 {
-    use Dispatchable, SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $post;
 
