aafc9063025e0f1c6aa7e48b962151548f7adf61
dkAsCambodia <nitindamo50@gmail.com>
2025-09-23 17:06:16 +0700

    work on show API documentation menu and show structure of API Docs

---
+++ resources/views/livewire/api-documentation.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h2>{{ __('messages.Api Documentation') }}</h2>
+    {{-- <embed src="{{ URL::to('pdf/RichPayDepositAPI.pdf') }}" type="application/pdf" width="100%" height="600px" /> --}}
+    <iframe src="{{ URL::to('pdf/RichPayDepositAPI.pdf') }}" width="100%" height="600px"></iframe>
+</div>
